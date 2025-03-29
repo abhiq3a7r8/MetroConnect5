52c483aecc0ee67ec9be7e80762618d39c26cf55
abhiq3a7r8 <audire444@gmail.com>
2025-03-29 14:36:03 +0530

    added login page logo

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,12 @@
-import { View , Text } from "react-native";
+import { View , Text , Image } from "react-native";
 import { LoginBox } from "@/components/LoginBox";
 
 
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-gray-100">
-        <Text className="color-black text-4xl mb-16 font-poppinsMedium">MetroConnect5</Text>
+        <Image source={require("@/assets/login.png")} className="mb-4"/>
+        <Text className="color-black text-4xl mb-16 font-poppinsMedium">MetroConnect</Text>
         <LoginBox />
     </View>
   );
--- components/LoginBox.tsx
+++ components/LoginBox.tsx
@@ -1,17 +1,20 @@
-import { View , Text } from "react-native"
+import { View, Text, TouchableOpacity } from "react-native";
 import CustomText from "./CustomText";
 import { MTextBox } from "./MTextBox";
 import { Mbutton } from "@/MButton";
 
 export function LoginBox() {
-    return(
-        <View className="bg-white h-96 w-[90%] rounded-[10] justify-evenly items-center ">
-            <CustomText className="color-black text-4xl">Login</CustomText>
+    return (
+        <View className="bg-white h-96 w-[90%] rounded-[10] justify-evenly items-center p-4">
+            <CustomText className="text-black text-4xl self-start ml-4">Login</CustomText>
             <MTextBox placeholder="Phone Number" />
             <MTextBox placeholder="Password" />
-            <Mbutton buttontext="Continue"/>            
+            <Mbutton buttontext="Continue" />
+            <TouchableOpacity>
+                <Text>Login with OTP</Text>
+            </TouchableOpacity>
         </View>
     );
 }
 
-export default LoginBox
+export default LoginBox;
